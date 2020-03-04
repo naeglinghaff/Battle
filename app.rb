db88f3fdca51eef5afb9824e9081873b6b08fd3a
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-04 09:26:36 +0000

    Added redirect feature to post play

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+enable :sessions
+
   get '/' do
     erb :index
   end
@@ -9,6 +11,14 @@ class Battle < Sinatra::Base
   post '/names' do
     @player_1_name = params[:player_1_name]
     @player_2_name = params[:player_2_name]
+    session[:player_1_name] = @player_1_name
+    session[:player_2_name] = @player_2_name
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
--- spec/features/testing_infra_spec.rb
+++ spec/features/testing_infra_spec.rb
@@ -2,6 +2,6 @@
 feature 'Testing infra' do
   scenario 'Can run the battle application and view page' do
     visit('/')
-    expect(page).to have_content 'Enter your name'
+    expect(page).to have_content 'Enter Player 1 and Player 2, please enter your names'
   end
 end
