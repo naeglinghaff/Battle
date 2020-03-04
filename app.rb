619adeb952d849aa356613726b03b7f9520b396a
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-04 09:43:36 +0000

    added feature tests for hit points

--- app.rb
+++ app.rb
@@ -2,12 +2,14 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+# lets you store information across post and get sessions
 enable :sessions
 
   get '/' do
     erb :index
   end
 
+# submits the name variables from params then puts them in the session hash, then redirects to new get view
   post '/names' do
     @player_1_name = params[:player_1_name]
     @player_2_name = params[:player_2_name]
@@ -16,9 +18,12 @@ enable :sessions
     redirect '/play'
   end
 
+# fetches params from session, gets new page
   get '/play' do
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
+    @player_1_hit = 50
+    @player_2_hit = 50
     erb :play
   end
 
