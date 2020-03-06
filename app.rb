9d0eecbb18d1d2dd3af80478a6b49dde71e99b70
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-06 12:08:52 +0000

    Removed global variables, class variables and methods introduced

--- app.rb
+++ app.rb
@@ -12,31 +12,44 @@ enable :sessions
   end
 
 # submits the name variables from params then puts them in the session hash, then redirects to new get view
-# now stores the vaiables in the object and accesses them via global variables
   post '/names' do
-    $game= Game.new(Player.new(params[:player_1_name]),Player.new(params[:player_2_name]))
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
-# fetches params from session, gets new page
-# now fetches from objects
+  before do
+    @game = Game.instance
+  end
+
   get '/play' do
-    @player_1_name = $game.player_1.name
-    @player_2_name = $game.player_2.name
-    @player_1_hp = $game.player_1.hp
-    @player_2_hp = $game.player_2.hp
-    erb :play
+    @player_1_name = @game.player_1.name
+    @player_2_name = @game.player_2.name
+    @player_2_hp = @game.player_2.hp
+    erb(:play)
   end
 
   get '/attack' do
-    $game.turn
-    @player_1_name = $game.player_1.name
-    @player_2_name = $game.player_2.name
-    @current_player_name = $game.current_player.name
-    @current_opponent_name = $game.current_opponent.name
-    @player_1_hp = $game.player_1.hp
-    @player_2_hp = $game.player_2.hp
-    erb :attack
+    @player_1_name = @game.player_1.name
+    @player_2_name = @game.player_2.name
+    @player_1_hp = @game.player_1.hp
+    @player_2_hp = @game.player_2.hp
+    @current_player = @game.current_player.name
+    erb(:attack)
+  end
+
+  post '/attack' do
+    @game.turn
+    if @game.game_over == true
+      redirect('/game_over')
+   else
+      redirect('/attack')
+    end
+  end
+
+  get '/game_over' do
+    'game over'
   end
 
   run! if app_file == $0
--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-attr_reader :player_1, :player_2, :current_player, :current_opponent
+attr_reader :player_1, :player_2, :current_player, :current_opponent, :game_over
 
   def initialize(player_1, player_2)
     @player_1 = player_1
@@ -8,23 +8,46 @@ attr_reader :player_1, :player_2, :current_player, :current_opponent
     @turn = true
     @current_player = @player_1
     @current_opponent = @player_2
+    @game_over = false
   end
 
   def turn
-    @turn ? attack(player_2) : attack(player_1)
+    if @player_1.hp == 0 || @player_2.hp == 0
+    end_game
+    else
+      @turn ? attack(@player_2) : attack(@player_1)
+    end
+  end
+
+  def self.create(player_1, player_2)
+    @game = Game.new(player_1, player_2)
+  end
+
+  def self.instance
+    @game
   end
 
 private
 
   def attack(player)
     player.reduce_hp
-    swap_turn
+    end_game
+    switch_current_player
   end
 
-  def swap_turn
+  def switch_turn
     @turn = !@turn
+  end
+
+  def switch_current_player
     @turn ? @current_player = @player_1 : @current_player = @player_2
     @turn ? @current_opponent = @player_2 : @current_opponent = @player_1
+    switch_turn
   end
 
+  def end_game
+    if @player_1.hp == 0 || @player_2.hp == 0
+      @game_over = @game_over = true
+    end
+  end
 end
