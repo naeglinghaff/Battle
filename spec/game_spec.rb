8a63de41ddeb667f04726c44f2f5e47a25650ed0
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-06 13:57:57 +0000

    Added unit tests for singleton principle

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,24 +15,28 @@ describe Game do
       expect(subject).to have_attributes(player_2: player_2)
     end
 
-    it '#turn' do
+    it 'should have turn variable' do
       expect(subject.instance_variable_get(:@turn)).to eql(true)
+    end
+
+    it 'should only be initialised via class methods' do
+      expect{Game.new}.to raise_error
     end
   end
 
   describe '#turn' do
-    it 'delegates to attack' do
+    it 'delegates to #attack' do
       expect(subject).to respond_to(:turn)
     end
 
-    it 'lets game over swap from true to false' do
+    it 'lets #game_over swap from true to false' do
       allow(player_1).to receive(:hp).and_return(0)
       expect { subject.turn }.to change { subject.game_over }.to eq true
     end
   end
 
   context 'lets players take turns' do
-    it '#turn delegates to attack' do
+    it '#turn swaps between players' do
       allow(player_1).to receive(:hp).and_return(50)
       allow(player_2).to receive(:hp).and_return(50)
       expect(subject).to receive(:attack).once
